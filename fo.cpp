daf08f6c4adfc9be04c495e58fc999ee44e9d9c9
Abhinandan Parewa <abhinandan.kr2005@gmail.com>
2026-04-06 17:12:29 +0530

    Update fo.cpp

---
+++ fo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+    public:
+        void pattern4(int N){
+            for (int i = 1; i <= N; i++){
+                for ( int j = 1; j <= i; j++){
+                    cout << i << " ";
+                }
+                cout << endl;
+            }
+        }
+};
+
+int main() {
+    solution sol;
+    int N = 5;
+    sol.pattern4(N);
+    return 0;
+}
